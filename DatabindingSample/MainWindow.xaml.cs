b55f1d21927f9a89d6b2afd3d740084df3a10f2b
Johann Grabner <jgrabner@business-tuning.at>
2024-01-25 14:22:19 +0100

    INotifyPropertyChanged

---
+++ DatabindingSample/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using DatabindingSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DatabindingSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Datenkontext auf ein Produkt setzen
+            Product myProduct = new Product();
+            myProduct.ProductId= 1;
+            myProduct.Productname = "Bleistift";
+            myProduct.Price = 1.99m;
+
+            myProduct =new Product() {
+                ProductId = 2,
+                Productname = "Radiergummi",
+                Price = 0.99m,
+            Color="Yellow",
+            Expired=true};
+
+            this.DataContext = myProduct;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Product myProduct = this.DataContext as Product;
+            var currentPrice = myProduct.Price;
+
+            myProduct.Price = currentPrice + 10;
+
+        }
+    }
+}
---
+++ DatabindingSample/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabindingSample.Models
+{
+    internal class Product: INotifyPropertyChanged //Observer Pattern
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int ProductId { get; set; }
+        public string Productname { get; set; }
+        //public decimal Price { get; set; } //Auto-Property
+
+        private decimal _Price;
+        public decimal Price
+        {
+            get { return _Price; }
+            set {
+                _Price = value;
+
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs("Price"));
+            }
+        }
+
+
+        public string Color { get; set; }
+        public bool Expired { get; set; }
+
+       
+    }
+}
