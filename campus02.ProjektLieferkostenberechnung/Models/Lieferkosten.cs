e3596fbeb08548106fd8474dbe223f6144bc25c0
Johann Grabner <jgrabner@business-tuning.at>
2024-02-01 12:55:16 +0100

    Properties und Berechnung

--- campus02.ProjektLieferkostenberechnung/Models/Lieferkosten.cs
+++ campus02.ProjektLieferkostenberechnung/Models/Lieferkosten.cs
@@ -33,6 +33,49 @@ namespace campus02.ProjektLieferkostenberechnung.Models
 
         public List<string> Laender { get; set; }
 
+        private string _AusgewLand;
+
+        public string AusgewLand
+        {
+            get { return _AusgewLand; }
+            set { 
+                _AusgewLand = value;
+                OnLandChange();
+            
+            }
+        }
+        private double _PreisProKilo;
+
+        public double PreisProKilo
+        {
+            get { return _PreisProKilo; }
+            set { _PreisProKilo = value; }
+        }
+        private string _Flagge;
+
+        public string Flagge
+        {
+            get { return _Flagge; }
+            set { _Flagge = value; }
+        }
+        private string _Ausgabetext;
+
+        public string Ausgabetext
+        {
+            get { return _Ausgabetext; }
+            set { _Ausgabetext = value; }
+        }
+
+
+
+
+
+        private void OnLandChange()
+        {
+            throw new NotImplementedException();
+        }
+
+
         #endregion
     }
 }
