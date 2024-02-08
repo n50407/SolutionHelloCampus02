93db79a813b1b762d674f429db2e2386f870a350
Johann Grabner <jgrabner@business-tuning.at>
2024-02-08 13:07:41 +0100

    Set DataContext

--- ProjektKinofilmverwaltung/MainWindow.xaml.cs
+++ ProjektKinofilmverwaltung/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjektKinofilmverwaltung.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,17 @@ namespace ProjektKinofilmverwaltung
         public MainWindow()
         {
             InitializeComponent();
+
+            KinofilmViewModel kinofilmViewModel = new KinofilmViewModel();
+
+            kinofilmViewModel.Kinofilme = new System.Collections.ObjectModel.ObservableCollection<Kinofilm>()
+            {
+                new Kinofilm() { FilmId = 1, Titel = "Star Wars", Genre = "Science Fiction", Bild = "starwars.jpg" },
+                new Kinofilm() { FilmId = 2, Titel = "Indiana Jones", Genre = "Abenteuer", Bild = "indianajones.jpg" },
+                new Kinofilm() { FilmId = 3, Titel = "James Bond", Genre = "Action", Bild = "jamesbond.jpg" }
+            };
+
+            this.DataContext = kinofilmViewModel;
         }
     }
 }
