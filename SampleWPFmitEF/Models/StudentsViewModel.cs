398fd34c62cd29e808dfd4fa59ccc3f4586d2687
Johann Grabner <jgrabner@business-tuning.at>
2024-02-09 10:02:12 +0100

    ViewModel mit EF-Context

---
+++ SampleWPFmitEF/Models/StudentsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWPFmitEF.Models
+{
+    internal class StudentsViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public StudentsViewModel()
+        {
+            MyStudents = new ObservableCollection<Student>();
+            var myStudentsLocalFromDB = db.Students.ToList();
+            foreach (var student in myStudentsLocalFromDB)
+            {
+                MyStudents.Add(student);
+            }
+            StatusAnzeige = "Momentan sind " + db.Students.ToList().Count + " Studierende in der DB";
+            OnPropertyChanged("StatusAnzeige");
+        }
+
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public Student NewStudent { get; set; }
+
+        public ObservableCollection<Student>  MyStudents { get; set; }
+
+        public string StatusAnzeige { get; set; }
+
+        Campus02DBContext db = new Campus02DBContext();
+        public void AddStudent(Student newStudent)
+        {
+            MyStudents.Add(newStudent);
+            //Check if exists
+            db.Students.Add(newStudent);
+            db.SaveChanges();
+            StatusAnzeige = "Momentan sind " + db.Students.ToList().Count + " Studierende in der DB";    
+            OnPropertyChanged("StatusAnzeige");
+
+        }
+    }
+}
